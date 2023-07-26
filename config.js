591c7e1f22adee7a227af25e5e242884a85335d6
nethmina234 <134161122+nethmina234@users.noreply.github.com>
2023-07-26 21:24:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,14 +5,14 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 
-global.SESSION_ID =  process.env.SESSION_ID || "Darknero=NrFGSajS#i-zLanEKF-5jSzKcvrS-Ua0c_x4t1RbAy1ZUmDbLku4"
+global.SESSION_ID =  process.env.SESSION_ID || "Darknero=li8wwIoQ#fBFr2ACDEvRjDWGO1KpZDXXhEKDCVT_rTqBkVziyRm0"
 
 module.exports = { 
 
  READ_MESSAGE : process.env. READ_MESSAGE  === undefined ?'true': process.env. READ_MESSAGE,
-AOTO_VOICS: process.env.AOTO_VOICS  === undefined ? 'false' : process.env.AOTO_VOICS, 
+AOTO_VOICS: process.env.AOTO_VOICS  === undefined ? 'true' : process.env.AOTO_VOICS, 
 YOU_LINK :  process.env.YOU_LINK  === undefined ? 'https://github.com/Kaveeshasithum' : process.env.YOU_LINK,
-OWENR_NO: process.env.OWENR_NO === undefined ? '94774071805' : process.env.OWENR_NO,
+OWENR_NO: process.env.OWENR_NO === undefined ? '94757182131' : process.env.OWENR_NO,
 ANTI_BAD: process.env.ANTI_BAD === undefined ? 'false' : process.env.ANTI_BAD,
 MAX_SIZE: 100,
 ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
